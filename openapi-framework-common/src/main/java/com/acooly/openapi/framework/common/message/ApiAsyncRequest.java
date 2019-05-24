c932c9cda2eccfcf38f3daa48ff25dc0441270c1
zhangpu <zhangpu@acooly.cn>
2019-05-24 18:04:16 +0800

    因马上停电，提交代码，用笔记本继续。

--- openapi-framework-common/src/main/java/com/acooly/openapi/framework/common/message/ApiAsyncRequest.java
+++ openapi-framework-common/src/main/java/com/acooly/openapi/framework/common/message/ApiAsyncRequest.java
@@ -1,16 +1,21 @@
 package com.acooly.openapi.framework.common.message;
 
+import com.acooly.openapi.framework.common.ApiConstants;
 import com.acooly.openapi.framework.common.annotation.OpenApiField;
 import lombok.Getter;
 import lombok.Setter;
 
-/** @author qiubo@yiji.com */
+/**
+ * @author qiubo@yiji.com
+ */
 @Getter
 @Setter
 public class ApiAsyncRequest extends ApiRequest {
-  @OpenApiField(desc = "通知地址", constraint = "使用异步服务时必填")
-  private String notifyUrl;
+    @OpenApiField(desc = "通知地址", constraint = "使用异步服务时必填",
+            demo = "http://xxx.xxx.com/notifyUrl.html", ordinal = ApiConstants.ORDINAL_MAX + 1)
+    private String notifyUrl;
 
-  @OpenApiField(desc = "回调地址", constraint = "需要回跳到商户时必填")
-  private String returnUrl;
+    @OpenApiField(desc = "回调地址", constraint = "需要回跳到商户时必填",
+            demo = "http://xxx.xxx.com/returnUrl.html", ordinal = ApiConstants.ORDINAL_MAX + 2)
+    private String returnUrl;
 }
--- openapi-framework-common/src/main/java/com/acooly/openapi/framework/common/message/AppRequest.java
+++ openapi-framework-common/src/main/java/com/acooly/openapi/framework/common/message/AppRequest.java
@@ -1,6 +1,9 @@
-/** create by zhangpu date:2015年5月12日 */
+/**
+ * create by zhangpu date:2015年5月12日
+ */
 package com.acooly.openapi.framework.common.message;
 
+import com.acooly.openapi.framework.common.ApiConstants;
 import com.acooly.openapi.framework.common.annotation.OpenApiField;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,19 +11,25 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Size;
 
-/** @author zhangpu */
+/**
+ * @author zhangpu
+ */
 @Getter
 @Setter
 public class AppRequest extends ApiRequest {
 
-  /** 设备唯一标识 */
-  @NotEmpty
-  @Size(min = 8, max = 128)
-  @OpenApiField(desc = "设备标识", constraint = "APP客户端设备唯一标识")
-  private String deviceId;
+    /**
+     * 设备唯一标识
+     */
+    @NotEmpty
+    @Size(min = 8, max = 128)
+    @OpenApiField(desc = "设备标识", constraint = "APP客户端设备唯一标识",
+            demo = "9774d56d682e549c", ordinal = ApiConstants.ORDINAL_MAX + 1)
+    private String deviceId;
 
-  @NotEmpty
-  @Size(min = 1, max = 10)
-  @OpenApiField(desc = "APP版本号", constraint = "APP程序版本号")
-  private String appVersion;
+    @NotEmpty
+    @Size(min = 1, max = 10)
+    @OpenApiField(desc = "APP版本号", constraint = "APP程序版本号",
+            demo = "111", ordinal = ApiConstants.ORDINAL_MAX + 2)
+    private String appVersion;
 }
