829165f991ec16ed81ed3b882d96c71b81801e37
zhangpu <zhangpu@prosysoft.com>
2019-05-28 18:37:28 +0800

    调整apidoc的数据库note字段的长度，增加到4000

--- openapi-framework-apidoc/src/main/resources/META-INF/database/mysql/apidoc.sql
+++ openapi-framework-apidoc/src/main/resources/META-INF/database/mysql/apidoc.sql
@@ -5,7 +5,7 @@ CREATE TABLE `api_doc_service` (
   `version` varchar(32) DEFAULT '1.0' COMMENT '服务版本',
   `title` varchar(256) NOT NULL DEFAULT '' COMMENT '服务标题',
   `owner` varchar(64) DEFAULT NULL COMMENT '所属系统',
-  `note` varchar(512) DEFAULT '' COMMENT '服务说明',
+  `note` varchar(4000) DEFAULT '' COMMENT '服务说明',
   `manual_note` varchar(512) DEFAULT '' COMMENT '手工说明',
   `service_type` varchar(64) NOT NULL DEFAULT '' COMMENT '服务类型{SYNC:同步}',
   `busi_type` varchar(32) DEFAULT NULL COMMENT '业务类型{Query:查询}',
@@ -16,7 +16,7 @@ CREATE TABLE `api_doc_service` (
   `signature` varchar(128) DEFAULT NULL COMMENT '签名(MD5(service_no+title+owner+note+service_type+busi_type))',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_API_DOC_SERVICE_NO` (`service_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=1726 DEFAULT CHARSET=utf8mb4 COMMENT='服务信息';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='服务信息';
 
 CREATE TABLE `api_doc_scheme` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -30,7 +30,7 @@ CREATE TABLE `api_doc_scheme` (
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `comments` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='服务方案';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='服务方案';
 
 CREATE TABLE `api_doc_scheme_desc` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -52,7 +52,7 @@ CREATE TABLE `api_doc_scheme_service` (
   `comments` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_API_SCHEME_SERVICE` (`scheme_no`,`service_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=185 DEFAULT CHARSET=utf8mb4 COMMENT='方案服务列表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='方案服务列表';
 
 CREATE TABLE `api_doc_message` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -68,7 +68,7 @@ CREATE TABLE `api_doc_message` (
   `signatrue` varchar(128) DEFAULT NULL COMMENT '签名(message_no+message_type+note)',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_API_DOC_MESSAGE_NO` (`message_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=3836 DEFAULT CHARSET=utf8mb4 COMMENT='服务报文';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='服务报文';
 
 CREATE TABLE `api_doc_item` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -94,7 +94,7 @@ CREATE TABLE `api_doc_item` (
   UNIQUE KEY `UK_api_doc_item` (`item_no`),
   KEY `FK_apidoc_item_self` (`parent_id`),
   CONSTRAINT `FK_apidoc_item_self` FOREIGN KEY (`parent_id`) REFERENCES `api_doc_item` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24999 DEFAULT CHARSET=utf8mb4 COMMENT='报文字段';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='报文字段';
 
 CREATE TABLE `api_meta_service` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -104,7 +104,7 @@ CREATE TABLE `api_meta_service` (
   `response_type` varchar(128) DEFAULT NULL COMMENT '服务响应类型',
   `owner` varchar(64) DEFAULT NULL COMMENT '所属系统',
   `busi_type` varchar(64) DEFAULT NULL COMMENT '服务名称',
-  `note` varchar(255) DEFAULT NULL COMMENT '服务介绍',
+  `note` varchar(4000) DEFAULT NULL COMMENT '服务介绍',
   `service_class` varchar(128) DEFAULT NULL COMMENT '服务类',
   `request_class` varchar(128) DEFAULT NULL COMMENT '请求类',
   `response_class` varchar(128) DEFAULT NULL COMMENT '同步响应类',
@@ -113,7 +113,7 @@ CREATE TABLE `api_meta_service` (
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_name_version` (`service_name`,`version`)
-) ENGINE=InnoDB AUTO_INCREMENT=1512 DEFAULT CHARSET=utf8mb4 COMMENT='服务类';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='服务类';
 
 CREATE TABLE `api_qa_classify` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
