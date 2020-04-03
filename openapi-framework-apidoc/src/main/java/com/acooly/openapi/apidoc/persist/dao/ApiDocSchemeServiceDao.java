494ca10f3ecdc90dfe97b0d265d8a7b65ab86412
lilin <lilin@cartechfin.com>
2020-04-03 15:30:51 +0800

    车云开放平台需求 api搜索按更新时间降序排列

--- openapi-framework-apidoc/src/main/java/com/acooly/openapi/apidoc/persist/dao/ApiDocSchemeServiceDao.java
+++ openapi-framework-apidoc/src/main/java/com/acooly/openapi/apidoc/persist/dao/ApiDocSchemeServiceDao.java
@@ -61,7 +61,7 @@ public interface ApiDocSchemeServiceDao extends EntityMybatisDao<ApiDocSchemeSer
     @Select("select t1.*,t3.scheme_no from api_doc_service as t1 join api_doc_scheme_service as t2 on t1.service_no=t2.service_no " +
             "join api_doc_scheme as t3 on t2.scheme_no= t3.scheme_no and t3.category='api' and t3.scheme_no!='SYSTEM' " +
             "and (upper(t1.name) like CONCAT(CONCAT('%', #{keywords}),'%') or UPPER(t1.title) like CONCAT(CONCAT('%', #{keywords}),'%')) " +
-            " order by t2.sort_time limit #{start},#{limit}")
+            " order by t1.update_time desc,t1.name limit #{start},#{limit}")
     List<ApiDocService> findContentServicesByKey(@Param("keywords") String keywords, @Param("start") int start, @Param("limit") int limit);
 
     @Select("select count(t1.id) from api_doc_service as t1 join api_doc_scheme_service as t2 on t1.service_no=t2.service_no " +
