be8c0101ae32cbd724c57567060a865405b66440
zhangpu <zhangpu@prosysoft.com>
2023-04-08 11:18:22 +0800

    增加权限初始化时，检查权重字符串格式的错误消息输出错误权限字符串的功能。

--- openapi-framework-core/src/main/java/com/acooly/openapi/framework/core/auth/permission/Permission.java
+++ openapi-framework-core/src/main/java/com/acooly/openapi/framework/core/auth/permission/Permission.java
@@ -10,26 +10,30 @@
  */
 package com.acooly.openapi.framework.core.auth.permission;
 
-import org.springframework.util.Assert;
-
 import static com.acooly.openapi.framework.common.ApiConstants.PERM_PATTERN;
 
-/** @author qiubo@qq.com */
+/**
+ * @author qiubo@qq.com
+ */
 public interface Permission {
-  /**
-   * 判断资源是否有权限.
-   *
-   * @param resource
-   * @return
-   */
-  boolean implies(String resource);
 
-  static void permMatch(String perm) {
-    Assert.state(
-        PERM_PATTERN.matcher(perm).matches(),
-        "权限字符串的格式应该为partnerId:serviceName,可以支持* :"
-            + "\n配置*,代表拥有所有权限."
-            + "\n 配置123:*,代表可以访问partnerId=123的所有服务"
-            + "\n 配置123:xxx*,代表可以访问partnerId=123,xxx开头的服务");
-  }
+    /**
+     * 判断设置的权限是否串是否合法
+     *
+     * @param perm
+     */
+    static void permMatch(String perm) {
+        if (!PERM_PATTERN.matcher(perm).matches()) {
+            throw new IllegalStateException("权限字符串的格式应该为partnerId:serviceName,可以支持*:，(例如：*:*,代表拥有所有权限; 配置123:*,代表可以访问partnerId=123的所有服务; 配置123:xxx*,代表可以访问partnerId=123,xxx开头的服务。perm：" + perm);
+        }
+//    Assert.state(PERM_PATTERN.matcher(perm).matches(), "权限字符串的格式应该为partnerId:serviceName,可以支持*:，(例如：*:*,代表拥有所有权限; 配置123:*,代表可以访问partnerId=123的所有服务; 配置123:xxx*,代表可以访问partnerId=123,xxx开头的服务");
+    }
+
+    /**
+     * 判断资源是否有权限.
+     *
+     * @param resource
+     * @return
+     */
+    boolean implies(String resource);
 }
